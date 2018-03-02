3ecc7c1cff87a8202283ea412f1ac457b01a12f9
xieyanan <xieyn66@163.com>
2018-03-02 17:29:40 +0800

    20180302 第一次修改

---
+++ src/main/java/com/xunsi/fs/main/FsServerApplication.java
@@ -0,0 +1,12 @@
+package com.xunsi.fs.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsServerApplication.class, args);
+	}
+}
