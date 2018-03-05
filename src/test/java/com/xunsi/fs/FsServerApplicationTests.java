7801fdc315470e0a30ac103733e50e3709c6f324
xieyanan <xieyn66@163.com>
2018-03-05 11:17:49 +0800

    20180305 testServer提交

--- src/test/java/com/xunsi/fs/FsServerApplicationTests.java
+++ src/test/java/com/xunsi/fs/FsServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FsserverApplicationTests {
+public class FsServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
